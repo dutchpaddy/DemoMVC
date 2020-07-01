bc182abec6e43bb98e8dd952b40b43b91ba452db
Eric van Berkel <vanberkel.eric@gmail.com>
2020-07-01 15:50:01 +0200

    Add files via upload

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private ActorDbContext _context;
+
+        public ActorsController( ActorDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View( _context.Actors.ToList());
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+            public IActionResult Create( Actors actors )
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(actors);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View( actors );
+                
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Delete(Actors actors)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Remove(actors);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(actors);
+
+        }
+
+    }
+}
---
+++ Models/ActorDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DemoMVC.Models
+{
+    public partial class ActorDbContext : DbContext
+    {
+        public ActorDbContext()
+        {
+        }
+
+        public ActorDbContext(DbContextOptions<ActorDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Actors> Actors { get; set; }
+
+ 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
